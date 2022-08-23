b0c275ac72e2b3e8f3a4e9cf838dd2bd698f8fd7
Andy Vo <47099119+andyqvo@users.noreply.github.com>
2022-08-23 08:30:07 -0700

    added icon

--- pihole.py
+++ pihole.py
@@ -13,82 +13,86 @@ DECIMAL_PLACES = os.getenv("DECIMAL_PLACES")
 TOKEN = os.getenv("TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
 
-PI_HOLE_ICON = "https://pbs.twimg.com/profile_images/917193425564209152/N9Eg5TQw_400x400.jpg"
+ICON_PATH = "src/icon.jpeg"
+ICON = open(ICON_PATH, "rb")
+ICON_BYTES = ICON.read()
 
 client = discord.Client()
 
 
 def fetch_info():
-  res = requests.get(f"{SERVER}/admin/api.php")
-  if res.status_code == 200:
-    return res.json()
+    res = requests.get(f"{SERVER}/admin/api.php")
+    if res.status_code == 200:
+        return res.json()
+
 
 def build_embed(title, description, color=0x239dd1):
-  embed = discord.Embed(title=title, description=description, color=color)
+    embed = discord.Embed(title=title, description=description, color=color)
 
-  now = datetime.now()
-  current_time = now.strftime("%H:%M:%S")
-  
-  embed.set_footer(text=f"Pi-Hole Discord | {current_time}")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
 
-  return embed
+    embed.set_footer(text=f"Pi-Hole Discord | {current_time}")
+
+    return embed
+
 
 async def help_command(channel_id):
-  embed = build_embed("All Commands", "`!stats - Get All Stats`")
-  await client.get_channel(channel_id).send(embed=embed)
+    embed = build_embed("All Commands", "`!stats - Get All Stats`")
+    await client.get_channel(channel_id).send(embed=embed)
 
 
 async def show_stats(channel_id):
-  info = fetch_info()
-  domains_being_blocked = "{:,}".format(info.get("domains_being_blocked"))
-  dns_queries_today = "{:,}".format(info.get("dns_queries_today"))
-  ads_blocked_today = "{:,}".format(info.get("ads_blocked_today"))
-  ads_percentage_today = "{:,.2f}".format(info.get("ads_percentage_today"))
+    info = fetch_info()
+    domains_being_blocked = "{:,}".format(info.get("domains_being_blocked"))
+    dns_queries_today = "{:,}".format(info.get("dns_queries_today"))
+    ads_blocked_today = "{:,}".format(info.get("ads_blocked_today"))
+    ads_percentage_today = "{:,.2f}".format(info.get("ads_percentage_today"))
 
-  description = ""
-  description += f"Total Queries: {dns_queries_today}\n"
-  description += f"Queries Blocked: {ads_blocked_today}\n"
-  description += f"Percentage Blocked: {ads_percentage_today}%\n"
-  description += f"Domains on Adlists: {domains_being_blocked}\n"
-  embed = build_embed(title="Stats", description=description)
+    description = ""
+    description += f"Total Queries: {dns_queries_today}\n"
+    description += f"Queries Blocked: {ads_blocked_today}\n"
+    description += f"Percentage Blocked: {ads_percentage_today}%\n"
+    description += f"Domains on Adlists: {domains_being_blocked}\n"
+    embed = build_embed(title="Stats", description=description)
 
-  await client.get_channel(channel_id).send(embed=embed)
+    await client.get_channel(channel_id).send(embed=embed)
 
 
 async def update_bot():
-  info = fetch_info()
-  ads_blocked_today = "{:,}".format(info.get("ads_blocked_today"))
-  ads_percentage_today = "{:,.2f}".format(info.get("ads_percentage_today"))
+    info = fetch_info()
+    ads_blocked_today = "{:,}".format(info.get("ads_blocked_today"))
+    ads_percentage_today = "{:,.2f}".format(info.get("ads_percentage_today"))
 
-  ads_blocked = f"Pi-Hole | {ads_blocked_today} ads blocked today."
+    ads_blocked = f"Pi-Hole | {ads_blocked_today} ads blocked today."
 
-  await client.get_guild(int(GUILD_ID)).me.edit(nick="Pi-Hole")
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=ads_blocked))
-  await asyncio.sleep(int(UPDATE_FREQUENCY))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=ads_blocked))
+    await asyncio.sleep(int(UPDATE_FREQUENCY))
 
 
 @client.event
 async def on_message(message):
-  commands = {
-    "!help": help_command,
-    "!stats": show_stats
-  }
+    commands = {
+        "!help": help_command,
+        "!stats": show_stats
+    }
 
-  content = message.content
-  channel_id = message.channel.id
+    content = message.content
+    channel_id = message.channel.id
 
-  if content in commands:
-    await commands[content](channel_id)
+    if content in commands:
+        await commands[content](channel_id)
 
 
 @client.event
 async def on_ready():
-  print(f"Logged in as Username: {client.user.name}")
-  print(f"User ID: {client.user.id}")
-  print("-----------")
+    print(f"Logged in as Username: {client.user.name}")
+    print(f"User ID: {client.user.id}")
+    print("-----------")
+    await client.user.edit(avatar=ICON)
 
-  while True:
-    await update_bot()
+    while True:
+        await update_bot()
 
 while True:
-  client.run(TOKEN)
+    client.run(TOKEN)
