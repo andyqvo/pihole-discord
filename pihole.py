6a5bee2ef248f0edd33b845ee576bc617af5dd3c
Andy Vo <47099119+andyqvo@users.noreply.github.com>
2022-08-23 07:51:46 -0700

    added bot commands to show stats and help

--- pihole.py
+++ pihole.py
@@ -31,6 +31,39 @@ async def update_bot():
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=bot_status_ads_percentage))
   await asyncio.sleep(int(UPDATE_FREQUENCY))
 
+async def help_command(channel_id):
+  embed = discord.Embed(title="All Commands", description="`!stats - Get All Stats`", color=0x239dd1)
+  await client.get_channel(channel_id).send(embed=embed)
+
+async def show_stats(channel_id):
+  info = fetch_info()
+  domains_being_blocked = info.get("domains_being_blocked")
+  dns_queries_today = info.get("dns_queries_today")
+  ads_blocked_today = info.get("ads_blocked_today")
+  ads_percentage_today = round(info.get("ads_percentage_today"), int(DECIMAL_PLACES))
+
+  description = ""
+  description += f"Domains being blocked: {domains_being_blocked}\n"
+  description += f"DNS queries today: {dns_queries_today}\n"
+  description += f"Ads blocked today: {ads_blocked_today}\n"
+  description += f"Ads percentage today: {ads_percentage_today}\n"
+  embed = discord.Embed(title="Stats", description=description, color=0x239dd1)
+
+  await client.get_channel(channel_id).send(embed=embed)
+
+@client.event
+async def on_message(message):
+  commands = {
+    "!help": help_command,
+    "!stats": show_stats
+  }
+
+  content = message.content
+  channel_id = message.channel.id
+
+  if content in commands:
+    await commands[content](channel_id)
+
 @client.event
 async def on_ready():
   print(f"Logged in as Username: {client.user.name}")
